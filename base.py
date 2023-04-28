016a6d669c207b89b4e6e3150953118adc3443c1
zhoujinlei <zhoujinlei@hortorgames.com>
2023-04-28 14:46:41 +0800

    feat: test

--- base.py
+++ base.py
@@ -1,6 +1,7 @@
 from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
+from textsplitter import ChineseTextSplitter
 from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 import os
 
@@ -10,12 +11,13 @@ loader = UnstructuredPDFLoader(file_path, mode="elements")
 
 # 定义文本分块的规则，这里用了一个很简单的规则，按照默认的分隔符来切割文本，使得每一段不超过1000个字符
 # splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-splitter = CharacterTextSplitter(separator="\n\n",
-                                 chunk_size=1000,
-                                 chunk_overlap=200,
-                                 length_function=len,
-                                 )
-chunks = loader.load_and_split(splitter)
+# splitter = CharacterTextSplitter(separator="\n\n",
+#                                  chunk_size=1000,
+#                                  chunk_overlap=200,
+#                                  length_function=len,
+#                                  )
+textsplitter = ChineseTextSplitter(pdf=True)
+chunks = loader.load_and_split(textsplitter)
 
 # 把文本转换为向量。
 embeddings = HuggingFaceEmbeddings(model_name="GanymedeNil/text2vec-large-chinese")
@@ -26,4 +28,4 @@ while True:
     query = input("Q：")
     result = indexStore.similarity_search(query)
     # for content in result
-    print('A', result)
+    print('A', result[0].page_content, result[1].page_content)
