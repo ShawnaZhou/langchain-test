505f88b50422bb0edbd0beda406ef258ee79192e
zhoujinlei <zhoujinlei@hortorgames.com>
2023-04-28 13:31:16 +0800

    feat: test

--- base.py
+++ base.py
@@ -2,13 +2,14 @@ from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
 
 # 加载文件
-loader = UnstructuredPDFLoader("test.pdf", mode="elements")
+file_path = os.path.abspath("/home/dev/model_inputs/retry/test.pdf")
+loader = UnstructuredPDFLoader(file_path, mode="elements")
 data = loader.load()
 print('dataLoaded', data)
 # 建立向量索引
-index = VectorstoreIndexCreator().from_loaders([loader])
+indexStore = VectorstoreIndexCreator().from_loaders([loader])
 
 while True:
-    query = input("Q：")
-    result = index.query_with_sources(query)
+    query = inputStore("Q：")
+    result = indexStore.query_with_sources(query)
     print('A：', result)
