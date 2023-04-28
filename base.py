f80dcbb54a190407329d0e5b99f0bc53372dd314
zhoujinlei <zhoujinlei@hortorgames.com>
2023-04-28 15:00:34 +0800

    feat: test

--- base.py
+++ base.py
@@ -10,13 +10,13 @@ file_path = os.path.abspath("/home/dev/model_inputs/retry/test.pdf")
 loader = UnstructuredPDFLoader(file_path, mode="elements")
 
 # 定义文本分块的规则，这里用了一个很简单的规则，按照默认的分隔符来切割文本，使得每一段不超过1000个字符
-# splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 # splitter = CharacterTextSplitter(separator="\n\n",
 #                                  chunk_size=1000,
 #                                  chunk_overlap=200,
 #                                  length_function=len,
 #                                  )
-textsplitter = ChineseTextSplitter(pdf=True)
+# textsplitter = ChineseTextSplitter(pdf=True)
 chunks = loader.load_and_split(textsplitter)
 
 # 把文本转换为向量。
@@ -29,4 +29,4 @@ while True:
     query = input("Q：")
     result = retriever.get_relevant_documents(query)
     # for content in result
-    print('A', result[0].page_content, result[1].page_content, result[2].page_content, result[3].page_content)
+    print('A', result)
