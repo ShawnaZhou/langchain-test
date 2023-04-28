0a4e5ac95a9b59bc1cccf5815be81533f2aa661c
zhoujinlei <zhoujinlei@hortorgames.com>
2023-04-28 15:02:47 +0800

    feat: test

--- base.py
+++ base.py
@@ -7,7 +7,7 @@ import os
 
 # 加载文件
 file_path = os.path.abspath("/home/dev/model_inputs/retry/test.pdf")
-loader = UnstructuredPDFLoader(file_path, mode="elements")
+loader = UnstructuredPDFLoader(file_path)
 
 # 定义文本分块的规则，这里用了一个很简单的规则，按照默认的分隔符来切割文本，使得每一段不超过1000个字符
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
