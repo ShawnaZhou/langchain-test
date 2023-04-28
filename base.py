25dc4e07f8a60de36299ac753783111de8d47951
zhoujinlei <zhoujinlei@hortorgames.com>
2023-04-28 15:05:57 +0800

    feat: test

--- base.py
+++ base.py
@@ -1,4 +1,5 @@
-from langchain.document_loaders import UnstructuredPDFLoader
+# from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.document_loaders import TextLoader, PyPDFLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from textsplitter import ChineseTextSplitter
@@ -7,7 +8,7 @@ import os
 
 # 加载文件
 file_path = os.path.abspath("/home/dev/model_inputs/retry/test.pdf")
-loader = UnstructuredPDFLoader(file_path)
+loader = file_path.endswith(".pdf") and PyPDFLoader(file_path) or TextLoader(file_path)
 
 # 定义文本分块的规则，这里用了一个很简单的规则，按照默认的分隔符来切割文本，使得每一段不超过1000个字符
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
